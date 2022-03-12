4c5ac11379e74f52ad2b7e2c4283c663fee5b0c8
Matk1p <rahmat329@gmail.com>
2022-03-12 20:03:33 +0800

    changed typos

--- D1/bandName.py
+++ D1/bandName.py
@@ -1,6 +1,7 @@
 # develop a band name generator to match the name of the city of the user and their pet's name
+print("Welcome to the band name generator.")
 
-cityName = input("What's your city name?\n")
-petName = input("What;s your pet name?\n")
+cityName = input("Which city did you grow up in?\n")
+petName = input("What is the name of your pet?\n")
 
-print("Your new band name is: " + cityName + " " + petName)
+print("Your band name could be " + cityName + " " + petName)
